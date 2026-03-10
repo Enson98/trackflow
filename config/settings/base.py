c060d9a5f8f04fde23397a12b19c2191f3937575
Antony Quintero <antonyquinterog@gmail.com>
2026-03-10 01:09:09 -0600

    fix: Correct BASE_DIR path

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 
-BASE_DIR = Path(_file_).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Apps instaladas
 INSTALLED_APPS = [
